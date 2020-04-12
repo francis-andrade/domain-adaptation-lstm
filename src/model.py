296175dcdefacc303925791993c7e0d8419496ce
francis-andrade <up201503481@fe.up.pt>
2020-04-12 17:04:59 +0100

    started adapting model

--- src/model.py
+++ src/model.py
@@ -1,40 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import torch
 import torch.nn as nn
-from torch.nn import init
+import torch.nn.functional as F
 
-class Flatten(nn.Module):
-    def forward(self, x):
-        N, C, H, W = x.size()
-        return x.view(N, -1) 
-    
-class Unflatten(nn.Module):
-    """
-    An Unflatten module receives an input of shape (N, C*H*W) and reshapes it
-    to produce an output of shape (N, C, H, W).
-    """
-    def __init__(self, N=-1, C=128, H=7, W=7):
-        super(Unflatten, self).__init__()
-        self.N = N
-        self.C = C
-        self.H = H
-        self.W = W
-    def forward(self, x):
-        return x.view(self.N, self.C, self.H, self.W)
 
-def initialize_weights(m):
-    if isinstance(m, nn.Linear) or isinstance(m, nn.ConvTranspose2d):
-        init.xavier_uniform_(m.weight.data)
 
-def discriminator():
+class GradientReversalLayer(torch.autograd.Function):
     """
-    Build and return a PyTorch model implementing the architecture above.
+    Implement the gradient reversal layer for the convenience of domain adaptation neural network.
+    The forward part is the identity function while the backward part is the negative function.
     """
-    model = nn.Sequential(
-            Flatten(),
-            nn.Linear(784, 256),
-            nn.CELU(alpha=0.01),
-            nn.Linear(256, 256),
-            nn.CELU(alpha=0.01),
-            nn.Linear(256, 1)
-    )
-    return model
+    @staticmethod
+    def forward(ctx, inputs):
+        return inputs
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = grad_output.clone()
+        grad_input = -grad_input
+        return grad_input
+
+class MDANet(nn.Module):
+    """
+    Multi-layer perceptron with adversarial regularizer by domain classification.
+    """
+    def __init__(self, configs):
+        super(MDANet, self).__init__()
+        self.input_dim = configs["input_dim"]
+        self.num_hidden_layers = len(configs["hidden_layers"])
+        self.num_neurons = [self.input_dim] + configs["hidden_layers"]
+        self.num_domains = configs["num_domains"]
+        # Parameters of hidden, fully-connected layers, feature learning component.
+        self.hiddens = nn.ModuleList([nn.Linear(self.num_neurons[i], self.num_neurons[i+1])
+                                      for i in range(self.num_hidden_layers)])
+        # Parameter of the final softmax classification layer.
+        self.softmax = nn.Linear(self.num_neurons[-1], configs["num_classes"])
+        # Parameter of the domain classification layer, multiple sources single target domain adaptation.
+        self.domains = nn.ModuleList([nn.Linear(self.num_neurons[-1], 2) for _ in range(self.num_domains)])
+        # Gradient reversal layer.
+        self.grls = [GradientReversalLayer() for _ in range(self.num_domains)]
+
+    def forward(self, sinputs, tinputs):
+        """
+        :param sinputs:     A list of k inputs from k source domains.
+        :param tinputs:     Input from the target domain.
+        :return:
+        """
+        sh_relu, th_relu = sinputs, tinputs
+        for i in range(self.num_domains):
+            for hidden in self.hiddens:
+                sh_relu[i] = F.relu(hidden(sh_relu[i]))
+        for hidden in self.hiddens:
+            th_relu = F.relu(hidden(th_relu))
+        # Classification probabilities on k source domains.
+        logprobs = []
+        for i in range(self.num_domains):
+            logprobs.append(F.log_softmax(self.softmax(sh_relu[i]), dim=1))
+        # Domain classification accuracies.
+        sdomains, tdomains = [], []
+        for i in range(self.num_domains):
+            sdomains.append(F.log_softmax(self.domains[i](self.grls[i].apply(sh_relu[i])), dim=1))
+            tdomains.append(F.log_softmax(self.domains[i](self.grls[i].apply(th_relu)), dim=1))
+        return logprobs, sdomains, tdomains
+
+    def inference(self, inputs):
+        h_relu = inputs
+        for hidden in self.hiddens:
+            h_relu = F.relu(hidden(h_relu))
+        # Classification probability.
+        logprobs = F.log_softmax(self.softmax(h_relu), dim=1)
+        return logprobs
+

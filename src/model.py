62ced027dafc30eae87ab844e6e4cf4f52e8cade
francis-andrade <up201503481@fe.up.pt>
2020-04-13 15:12:53 +0100

    adapted model to images

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from collections import OrderedDict
 
 
 class GradientReversalLayer(torch.autograd.Function):
@@ -42,13 +42,86 @@ class MDANet(nn.Module):
         # Gradient reversal layer.
         self.grls = [GradientReversalLayer() for _ in range(self.num_domains)]
 
-    def forward(self, sinputs, tinputs):
+        self.conv_blocks = nn.ModuleList()
+        self.conv_blocks.append(
+            nn.Sequential(OrderedDict([
+                ('Conv1_1', nn.Conv2d(3, 64, (3, 3), padding=1)),
+                ('ReLU1_1', nn.ReLU()),
+                ('Conv1_2', nn.Conv2d(64, 64, (3, 3), padding=1)),
+                ('ReLU1_2', nn.ReLU()),
+                ('MaxPool1', nn.MaxPool2d((2, 2))),
+                ('Conv2_1', nn.Conv2d(64, 128, (3, 3), padding=1)),
+                ('ReLU2_1', nn.ReLU()),
+                ('Conv2_2', nn.Conv2d(128, 128, (3, 3), padding=1)),
+                ('ReLU2_2', nn.ReLU()),
+                ('MaxPool2', nn.MaxPool2d((2, 2))),
+            ])))
+        self.conv_blocks.append(
+            nn.Sequential(OrderedDict([
+                ('Conv3_1', nn.Conv2d(128, 256, (3, 3), padding=1)),
+                ('ReLU3_1', nn.ReLU()),
+                ('Conv3_2', nn.Conv2d(256, 256, (3, 3), padding=1)),
+                ('ReLU3_2', nn.ReLU()),
+                ('Atrous1', nn.Conv2d(256, 256, (3, 3), dilation=2, padding=2)),
+                ('ReLU_A1', nn.ReLU()),
+            ])))
+        self.conv_blocks.append(
+            nn.Sequential(OrderedDict([
+                ('Conv4_1', nn.Conv2d(256, 256, (3, 3), padding=1)),
+                ('ReLU4_1', nn.ReLU()),
+                ('Conv4_2', nn.Conv2d(256, 256, (3, 3), padding=1)),
+                ('ReLU4_2', nn.ReLU()),
+                ('Atrous2', nn.Conv2d(256, 512, (3, 3), dilation=2, padding=2)),
+                ('ReLU_A2', nn.ReLU()),
+            ])))
+        self.conv_blocks.append(
+            nn.Sequential(OrderedDict([
+                ('Atrous3', nn.Conv2d(512, 512, (3, 3), dilation=2, padding=2)),
+                ('ReLU_A3', nn.ReLU()),
+                ('Atrous4', nn.Conv2d(512, 512, (3, 3), dilation=2, padding=2)),
+                ('ReLU_A4', nn.ReLU()),
+            ])))
+
+    def forward_single_input(self, input, mask = None):
+        
+        if mask is not None:
+            input = input * mask  # zero input values outside the active region
+
+        h1 = self.conv_blocks[0](input)
+        h2 = self.conv_blocks[1](h1)
+        h3 = self.conv_blocks[2](h2)
+        h4 = self.conv_blocks[3](h3)
+        h = torch.cat((h1, h2, h3, h4), dim=1)  # hyper-atrous combination
+        h = self.conv_blocks[4](h)
+        if mask is not None:
+            h = h * mask  # zero output values outside the active region
+
+        density = h  # predicted density map
+        count = h.sum(dim=(1, 2, 3))  # predicted vehicle count
+
+        return density, count
+
+    def forward(self, sinputs, tinputs, mask=None):
         """
         :param sinputs:     A list of k inputs from k source domains.
         :param tinputs:     Input from the target domain.
         :return:
         """
-        sh_relu, th_relu = sinputs, tinputs
+
+        sdensity = []
+        scount = []
+        for i in range(self.num_domains):
+            density, count = self.forward_single_input(sinputs[i], mask)
+            sdensity.append(density)
+            scount.append(count)
+
+        tdensity, tcount = self.forward_single_input(tinputs)
+
+        sdomains, tdomains = [], []
+        for i in range(self.num_domains):
+            sdomains.append(F.log_softmax(self.domains[i](self.grls[i].apply(sdensity[i])), dim=1))
+            tdomains.append(F.log_softmax(self.domains[i](self.grls[i].apply(tdensity))))
+
         for i in range(self.num_domains):
             for hidden in self.hiddens:
                 sh_relu[i] = F.relu(hidden(sh_relu[i]))

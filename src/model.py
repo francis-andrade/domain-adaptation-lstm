corrected gradient reversal in models

src/model.py
@@ -131,8 +131,8 @@ class MDANet(nn.Module):
 
         sdomains, tdomains = [], []
         for i in range(self.num_domains):
-            sdomains.append(F.log_softmax(self.domains[i](self.grls[i].apply(self.flatten[i](sh[i]))), dim=1))
-            tdomains.append(F.log_softmax(self.domains[i](self.grls[i].apply(self.flatten[i](th)))))
+            sdomains.append(F.log_softmax(self.domains[i](self.flatten[i](self.grls[i].apply(sh[i]))), dim=1))
+            tdomains.append(F.log_softmax(self.domains[i](self.flatten[i](self.grls[i].apply(th))), dim=1))
 
         return sdensity, scount, sdomains, tdomains
     

src/model_temporal_double.py
@@ -49,16 +49,18 @@ class MDANTemporalDouble(MDANTemporal):
 
         sdomains, tdomains = [], []
         for i in range(self.num_domains):
+            h_lstm = self.grls[i].apply(sh[i])
             h0 = self.init_hidden(N)
             h_lstm = self.domains_conv[i](sh[i])
             h_lstm = h_lstm.reshape(N, T, -1)
             h_lstm, _ = self.domains_lstm[i](h_lstm, h0)
-            sdomains.append(F.log_softmax(self.domains[i](self.grls[i].apply(self.flatten[i](h_lstm))), dim=1))
+            sdomains.append(F.log_softmax(self.domains[i](self.flatten[i](h_lstm)), dim=1))
+            h_lstm = self.grls[i].apply(sh[i])
             h0 = self.init_hidden(N)
             h_lstm = self.domains_conv[i](th)
             h_lstm = h_lstm.reshape(N, T, -1)
             h_lstm, _ = self.domains_lstm[i](h_lstm, h0)
-            tdomains.append(F.log_softmax(self.domains[i](self.grls[i].apply(self.flatten[i](h_lstm)))))
+            tdomains.append(F.log_softmax(self.domains[i](self.flatten[i](h_lstm)), dim=1))
 
         return sdensity, scount, sdomains, tdomains
         

src/run.py
@@ -172,7 +172,7 @@ for i in range(settings.NUM_DATASETS):
             else:
                 train_loader = utils.multi_data_loader(data_insts, data_densities, data_counts, batch_size)
             for batch_insts, batch_densities, batch_counts in train_loader:
-                logger.info("Starting batch")
+                #logger.info("Starting batch")
                 # Build source instances.
                 source_insts = []
                 source_counts = []
@@ -195,7 +195,7 @@ for i in range(settings.NUM_DATASETS):
                 for k in range(num_domains):
                     slabels.append(torch.ones(len(source_insts[k]), requires_grad=False).type(torch.LongTensor).to(device))
                     tlabels.append(torch.zeros(len(source_insts[k]), requires_grad=False).type(torch.LongTensor).to(device))
-                print("Starting MDAN")
+                #print("Starting MDAN")
                 model_densities, model_counts, sdomains, tdomains = mdan(source_insts, tinputs)
                 # Compute prediction accuracy on multiple training sources.
                 density_losses = torch.stack([(torch.sum((model_densities[j] - source_densities[j])**2)/(len(model_densities[j]))) for j in range(num_domains)])